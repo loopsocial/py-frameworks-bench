8551939b1af193802349605febef14f7958113e4
Jack Deng <jack@fireworkhq.com>
2022-11-21 15:39:36 +0800

    try uvicorn with fastapi
    Signed-off-by: Jack Deng <jack@fireworkhq.com>

--- frameworks/fastapi/start.sh
+++ frameworks/fastapi/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/usr/local/bin/gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 app:app
+# /usr/local/bin/gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 app:app
+uvicorn --host 0.0.0.0 --port 8080 app:app
